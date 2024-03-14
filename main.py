c7a5570c70f69208710a2ecd7d140f4f4a35fe3b
eval-u8 <evalecillos@gmail.com>
2024-03-13 21:40:47 -0500

    add main fn, content of book and word count functions

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hola")
+def main():
+    book_path = "./books/frankenstein.txt"
+    book_text = get_book_content(book_path)
+    book_word_count = get_book_word_count(book_text)
+    print(f"This book has {book_word_count} words!")
+
+def get_book_content(book_itself):
+    with open(book_itself) as libro:
+        return libro.read()
+
+def get_book_word_count(book_text):
+    data = book_text.split()
+    return len(data)
+
+main()
