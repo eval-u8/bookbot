8463a60002b30fe5c99591f6181b39372d43c1b2
eval-u8 <evalecillos@gmail.com>
2024-03-13 23:01:00 -0500

    refactoring some of the functions, and fixing the logic behind the if statement on the repeat characters function

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 def main():
     book_path = "./books/frankenstein.txt"
-    book_text = get_book_content(book_path)
-    book_word_count = get_book_word_count(book_text)
-    print(get_repeat_chars(book_text))
+    book_content = get_book_content(book_path)
+    book_word_count = get_book_word_count(book_content)
+    repeat_characters = get_repeat_chars(book_content)
+    print(time_to_report(book_path, book_word_count, repeat_characters))
 
-#defining fn that will read the file from its folder and return it
-def get_book_content(book_itself):
-    with open(book_itself) as libro:
-        return libro.read()
 
 #defining fn that will return the word count of the whole book
 def get_book_word_count(book_text):
     data = book_text.split()
     return len(data)
 
+
+def time_to_report(path, word_count, char_count):
+    text = f"-- Begin report of {path} -- \n {word_count} words found in the document \n\n"
+    char_count_list = list(char_count.items())
+    sorted_list = sorted(char_count_list, key=lambda x: x[1], reverse=True)
+    for i in sorted_list:
+        text += f"The '{i[0]}' character was found {i[1]} times\n"
+    text= f"{text} === End Report ==="
+    return text
+
 #defining fn that will return the number of times each character appears in the book
 def get_repeat_chars(book_text):
-    #this way it won't show the 'enters' aka '\n' but it can also be done excluding the next two lines and passing 'book_text' instead of the 'raw_string' in the for loop
-    raw_chars_array = book_text.split()
-    raw_string = "".join(raw_chars_array)
     char_dict = {}
-    for char in raw_string:
+    for char in book_text:
         lowered = char.lower()
-        if char in char_dict:
+        if not lowered.isalpha():
+            pass
+        elif lowered in char_dict:
             char_dict[lowered] += 1
         else:
             char_dict[lowered] = 1
     return(char_dict)
+
+#defining fn that will read the file from its folder and return it
+def get_book_content(book_itself):
+    with open(book_itself) as libro:
+        return libro.read()
 
 main()
